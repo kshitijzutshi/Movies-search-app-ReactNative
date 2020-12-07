c537e35f4b6bca04755ef8521c71359ec0a9f78a
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-12-07 10:53:35 +0530

    movie db app using flatlist and animations

--- App.js
+++ App.js
@@ -1,230 +1,339 @@
-import axios from 'axios';
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, Image, TouchableHighlight, Modal } from 'react-native';
+/**
+ * Inspiration: https://dribbble.com/shots/8257559-Movie-2-0
+ *
+ */
+import * as React from 'react';
+import {
+  StatusBar,
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Image,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+const { width, height } = Dimensions.get('window');
+import { getMovies } from './api';
+import Genres from './Genres';
+import Rating from './Rating';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const SPACING = 10;
+const ITEM_SIZE = Platform.OS === 'ios' ? width * 0.72 : width * 0.74;
+const EMPTY_ITEM_SIZE = (width - ITEM_SIZE) / 2;
+const BACKDROP_HEIGHT = height * 0.65;
+
+const Loading = () => (
+  <View style={styles.loadingContainer}>
+    <Text style={styles.paragraph}>Loading...</Text>
+  </View>
+);
+
+const Backdrop = ({ movies, scrollX }) => {
+  return (
+    <View style={{ height: BACKDROP_HEIGHT, width, position: 'absolute' }}>
+      <FlatList
+        data={movies.reverse()}
+        keyExtractor={(item) => item.key + '-backdrop'}
+        removeClippedSubviews={false}
+        contentContainerStyle={{ width, height: BACKDROP_HEIGHT }}
+        renderItem={({ item, index }) => {
+          if (!item.backdrop) {
+            return null;
+          }
+          const translateX = scrollX.interpolate({
+            inputRange: [(index - 2) * ITEM_SIZE, (index - 1) * ITEM_SIZE],
+            outputRange: [0, width],
+            // extrapolate:'clamp'
+          });
+          return (
+            <Animated.View
+              removeClippedSubviews={false}
+              style={{
+                position: 'absolute',
+                width: translateX,
+                height,
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                source={{ uri: item.backdrop }}
+                style={{
+                  width,
+                  height: BACKDROP_HEIGHT,
+                  position: 'absolute',
+                }}
+              />
+            </Animated.View>
+          );
+        }}
+      />
+      <LinearGradient
+        colors={['rgba(0, 0, 0, 0)', 'white']}
+        style={{
+          height: BACKDROP_HEIGHT,
+          width,
+          position: 'absolute',
+          bottom: 0,
+        }}
+      />
+    </View>
+  );
+};
 
 export default function App() {
-  const apiURL = "http://www.omdbapi.com/?apikey=82f423db";
-  const [state, setState] = useState({
-    s: "Enter a movie..",
-    results: [],
-    selected: {}
-  })
+  const [movies, setMovies] = React.useState([]);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const movies = await getMovies();
+      // Add empty items to create fake space
+      // [empty_item, ...movies, empty_item]
+      setMovies([{ key: 'empty-left' }, ...movies, { key: 'empty-right' }]);
+    };
 
-  const search = () => {
-    axios(apiURL + "&s=" + state.s).then( ({data}) => {
-      let results = data.Search
-      setState(prevState => {
-        return { ...prevState, results: results}
-      })
-    })
-  }
-// tt2975590
-  const openPopup = (id) => {
-    axios(apiURL + "&i=" + id).then( ({data}) => {
-      let result = data
-      console.log(result);
-      setState(prevState => {
-        return { ...prevState, selected: result}
-      })
-    })
+    if (movies.length === 0) {
+      fetchData(movies);
+    }
+  }, [movies]);
+
+  if (movies.length === 0) {
+    return <Loading />;
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>MovieDB Search</Text>
-      <TextInput
-      style={styles.searchBox}
-      onChangeText= { text => setState(prevState => {
-        return {...prevState, s: text}
-      })}
-      onSubmitEditing={search}
-      value={state.s}
-      />
-      <ScrollView styles = {styles.results}>
-      
-      {state.results.map(result => {
-        return(
-          <TouchableHighlight key={result.imdbID} onPress={() => openPopup(result.imdbID)}>
-            <View style={styles.result}>
-              <Image source={{ uri: result.Poster}} style={{ width: '100%', height: 300}} resizeMode="cover" />
-              <Text style={styles.heading}>{result.Title}</Text>
-            </View>
-          </TouchableHighlight>
-        
-        )
-      })}
-      </ScrollView>
-      <Modal animationType="fade" transparent={false} visible={(typeof state.selected.Title != "undefined")}>
-        <View style={styles.popup}>
-        <Text style={styles.poptitle}>{state.selected.Title}</Text>
-        <Text style={{marginBottom: 20}}>Rating: {state.selected.imdbRating}</Text>
-        <Text>{state.selected.Plot}</Text>
-        </View>
-        
-        <TouchableHighlight onPress={() => setState(prevState => {
-          return {...prevState, selected: {}}
-        })}>
-          <Text style={styles.closeBtn}>Close</Text>
-        </TouchableHighlight>
+      <Backdrop movies={movies} scrollX={scrollX} />
+      <StatusBar hidden />
+      <Animated.FlatList
+        showsHorizontalScrollIndicator={false}
+        data={movies}
+        keyExtractor={(item) => item.key}
+        horizontal
+        bounces={false}
+        decelerationRate={Platform.OS === 'ios' ? 0 : 0.98}
+        renderToHardwareTextureAndroid
+        contentContainerStyle={{ alignItems: 'center' }}
+        snapToInterval={ITEM_SIZE}
+        snapToAlignment='start'
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        scrollEventThrottle={16}
+        renderItem={({ item, index }) => {
+          if (!item.poster) {
+            return <View style={{ width: EMPTY_ITEM_SIZE }} />;
+          }
 
-      </Modal>
-      
+          const inputRange = [
+            (index - 2) * ITEM_SIZE,
+            (index - 1) * ITEM_SIZE,
+            index * ITEM_SIZE,
+          ];
+
+          const translateY = scrollX.interpolate({
+            inputRange,
+            outputRange: [100, 50, 100],
+            extrapolate: 'clamp',
+          });
+
+          return (
+            <View style={{ width: ITEM_SIZE }}>
+              <Animated.View
+                style={{
+                  marginHorizontal: SPACING,
+                  padding: SPACING * 2,
+                  alignItems: 'center',
+                  transform: [{ translateY }],
+                  backgroundColor: 'white',
+                  borderRadius: 34,
+                }}
+              >
+                <Image
+                  source={{ uri: item.poster }}
+                  style={styles.posterImage}
+                />
+                <Text style={{ fontSize: 24 }} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <Rating rating={item.rating} />
+                <Genres genres={item.genres} />
+                <Text style={{ fontSize: 12 }} numberOfLines={3}>
+                  {item.description}
+                </Text>
+              </Animated.View>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#223343',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    paddingTop: 70,
-    paddingHorizontal: 20
   },
-  title: {
-    fontSize: 30,
-    color: "#FFF",
-    textAlign: "center",
-    fontWeight: '700',
-    marginBottom: 20,
-    width: "100%"
-  },
-  searchBox:{
-    fontSize: 20,
-    fontWeight: '300',
-    padding: 20,
-    width: '100%',
-    backgroundColor: '#FFF',
-    borderRadius: 8,
-    marginBottom: 40
-  },
-  results: {
-    flex: 1
-  },
-  result: {
-    flex: 1,
-    width: "100%",
-    marginBottom: 20
-  },
-  heading: {
-    color: "#FFF",
+  paragraph: {
+    margin: 24,
     fontSize: 18,
-    fontWeight: '700',
-    padding: 20,
-    backgroundColor: '#445565'
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  popup: {
-    padding: 20
+  posterImage: {
+    width: '100%',
+    height: ITEM_SIZE * 1.2,
+    resizeMode: 'cover',
+    borderRadius: 24,
+    margin: 0,
+    marginBottom: 10,
   },
-  poptitle: {
-    fontSize: 24,
-    fontWeight: '700',
-    marginBottom: 5
-  },
-  closeBtn: {
-    padding: 20,
-    fontSize: 20,
-    fontWeight: '700',
-    backgroundColor: "#2484C4",
-    color: "#FFF",
-    textAlign: "center",
-    justifyContent: "center",
-    width: '100%'
-  }
 });
 
 
 
+// import axios from 'axios';
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState } from 'react';
+// import { StyleSheet, Text, View, TextInput, ScrollView, Image, TouchableHighlight, Modal } from 'react-native';
 
-// import React, { useState } from "react";
-// import {
-//   Alert,
-//   Modal,
-//   StyleSheet,
-//   Text,
-//   TouchableHighlight,
-//   View
-// } from "react-native";
+// export default function App() {
+//   const apiURL = "http://www.omdbapi.com/?apikey=82f423db";
+//   const [state, setState] = useState({
+//     s: "Enter a movie..",
+//     results: [],
+//     selected: {}
+//   })
 
-// const App = () => {
-//   const [modalVisible, setModalVisible] = useState(false);
+//   const search = () => {
+//     axios(apiURL + "&s=" + state.s).then( ({data}) => {
+//       let results = data.Search
+//       setState(prevState => {
+//         return { ...prevState, results: results}
+//       })
+//     })
+//   }
+// // tt2975590
+//   const openPopup = (id) => {
+//     axios(apiURL + "&i=" + id).then( ({data}) => {
+//       let result = data
+//       console.log(result);
+//       setState(prevState => {
+//         return { ...prevState, selected: result}
+//       })
+//     })
+//   }
+
 //   return (
-//     <View style={styles.centeredView}>
-//       <Modal
-//         animationType="slide"
-//         transparent={true}
-//         visible={modalVisible}
-//         onRequestClose={() => {
-//           Alert.alert("Modal has been closed.");
-//         }}
-//       >
-//         <View style={styles.centeredView}>
-//           <View style={styles.modalView}>
-//             <Text style={styles.modalText}>Hello World!</Text>
-
-//             <TouchableHighlight
-//               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-//               onPress={() => {
-//                 setModalVisible(!modalVisible);
-//               }}
-//             >
-//               <Text style={styles.textStyle}>Hide Modal</Text>
-//             </TouchableHighlight>
-//           </View>
+//     <View style={styles.container}>
+//       <Text style={styles.title}>MovieDB Search</Text>
+//       <TextInput
+//       style={styles.searchBox}
+//       onChangeText= { text => setState(prevState => {
+//         return {...prevState, s: text}
+//       })}
+//       onSubmitEditing={search}
+//       value={state.s}
+//       />
+//       <ScrollView styles = {styles.results}>
+      
+//       {state.results.map(result => {
+//         return(
+//           <TouchableHighlight key={result.imdbID} onPress={() => openPopup(result.imdbID)}>
+//             <View style={styles.result}>
+//               <Image source={{ uri: result.Poster}} style={{ width: '100%', height: 300}} resizeMode="cover" />
+//               <Text style={styles.heading}>{result.Title}</Text>
+//             </View>
+//           </TouchableHighlight>
+        
+//         )
+//       })}
+//       </ScrollView>
+//       <Modal animationType="fade" transparent={false} visible={(typeof state.selected.Title != "undefined")}>
+//         <View style={styles.popup}>
+//         <Text style={styles.poptitle}>{state.selected.Title}</Text>
+//         <Text style={{marginBottom: 20}}>Rating: {state.selected.imdbRating}</Text>
+//         <Text>{state.selected.Plot}</Text>
 //         </View>
-//       </Modal>
+        
+//         <TouchableHighlight onPress={() => setState(prevState => {
+//           return {...prevState, selected: {}}
+//         })}>
+//           <Text style={styles.closeBtn}>Close</Text>
+//         </TouchableHighlight>
 
-//       <TouchableHighlight
-//         style={styles.openButton}
-//         onPress={() => {
-//           setModalVisible(true);
-//         }}
-//       >
-//         <Text style={styles.textStyle}>Show Modal</Text>
-//       </TouchableHighlight>
+//       </Modal>
+      
 //     </View>
 //   );
-// };
+// }
 
 // const styles = StyleSheet.create({
-//   centeredView: {
+//   container: {
 //     flex: 1,
+//     backgroundColor: '#223343',
+//     alignItems: 'center',
+//     justifyContent: 'flex-start',
+//     paddingTop: 70,
+//     paddingHorizontal: 20
+//   },
+//   title: {
+//     fontSize: 30,
+//     color: "#FFF",
+//     textAlign: "center",
+//     fontWeight: '700',
+//     marginBottom: 20,
+//     width: "100%"
+//   },
+//   searchBox:{
+//     fontSize: 20,
+//     fontWeight: '300',
+//     padding: 20,
+//     width: '100%',
+//     backgroundColor: '#FFF',
+//     borderRadius: 8,
+//     marginBottom: 40
+//   },
+//   results: {
+//     flex: 1
+//   },
+//   result: {
+//     flex: 1,
+//     width: "100%",
+//     marginBottom: 20
+//   },
+//   heading: {
+//     color: "#FFF",
+//     fontSize: 18,
+//     fontWeight: '700',
+//     padding: 20,
+//     backgroundColor: '#445565'
+//   },
+//   popup: {
+//     padding: 20
+//   },
+//   poptitle: {
+//     fontSize: 24,
+//     fontWeight: '700',
+//     marginBottom: 5
+//   },
+//   closeBtn: {
+//     padding: 20,
+//     fontSize: 20,
+//     fontWeight: '700',
+//     backgroundColor: "#2484C4",
+//     color: "#FFF",
+//     textAlign: "center",
 //     justifyContent: "center",
-//     alignItems: "center",
-//     marginTop: 22
-//   },
-//   modalView: {
-//     margin: 20,
-//     backgroundColor: "white",
-//     borderRadius: 20,
-//     padding: 35,
-//     alignItems: "center",
-//     shadowColor: "#000",
-//     shadowOffset: {
-//       width: 0,
-//       height: 2
-//     },
-//     shadowOpacity: 0.25,
-//     shadowRadius: 3.84,
-//     elevation: 5
-//   },
-//   openButton: {
-//     backgroundColor: "#F194FF",
-//     borderRadius: 20,
-//     padding: 10,
-//     elevation: 2
-//   },
-//   textStyle: {
-//     color: "white",
-//     fontWeight: "bold",
-//     textAlign: "center"
-//   },
-//   modalText: {
-//     marginBottom: 15,
-//     textAlign: "center"
+//     width: '100%'
 //   }
 // });
-
-// export default App;
---
+++ config.js
@@ -0,0 +1,1 @@
+export const API_KEY = '13a7b7101b709842af470b7c7bff0a45';
